e923733064d3117e3d06826f1ff345eada119737
Adam Byrne <adamrbyrne@gmail.com>
2025-11-04 09:56:06 +0000

    fix(evolution): parallel'd build pop

--- internal/evolution/population.go
+++ internal/evolution/population.go
@@ -2,6 +2,8 @@ package evolution
 
 import (
 	"github.com/bxrne/darwin/internal/individual"
+	"runtime"
+	"sync"
 )
 
 // PopulationBuilder creates initial populations
@@ -15,8 +17,24 @@ func NewPopulationBuilder() *PopulationBuilder {
 // BuildBinaryPopulation creates a population of binary individuals
 func (pb *PopulationBuilder) BuildBinaryPopulation(size, genomeSize int) []individual.Evolvable {
 	population := make([]individual.Evolvable, size)
-	for i := range population {
-		population[i] = individual.NewBinaryIndividual(genomeSize)
+	var wg sync.WaitGroup
+	numWorkers := runtime.NumCPU()
+
+	chunkSize := (size + numWorkers - 1) / numWorkers
+	for i := range numWorkers {
+		start := i * chunkSize
+		end := start + chunkSize
+		end = min(end, size)
+
+		wg.Add(1)
+		go func(start, end int) {
+			defer wg.Done()
+			for j := start; j < end; j++ {
+				population[j] = individual.NewBinaryIndividual(genomeSize)
+			}
+		}(start, end) // chunk to use
 	}
+
+	wg.Wait()
 	return population
 }
