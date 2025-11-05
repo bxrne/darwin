4c59c2dbf650cc5710fac2a848fd05ef440b81e2
ArtLiathain <art.oliathain@gmail.com>
2025-11-05 14:32:21 +0000

    fix(tree): resolve merge conflicts

--- internal/individual/evolvable.go
+++ internal/individual/evolvable.go
@@ -2,9 +2,9 @@ package individual
 
 // Evolvable represents an individual that can evolve through genetic operations
 type Evolvable interface {
-	CalculateFitness()
 	Mutate(rate float64)
-	GetFitness() float64
 	Max(i2 Evolvable) Evolvable
 	MultiPointCrossover(i2 Evolvable, crossoverPointCount int) (Evolvable, Evolvable)
+	GetFitness() float64
+	SetFitness(fitness float64)
 }
