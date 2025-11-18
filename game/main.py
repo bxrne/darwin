1ddd617abf943d70ce511aef3ec03eb3d98ab4c1
Adam Byrne <adamrbyrne@gmail.com>
2025-11-18 13:36:18 +0000

    chore(game): added generals usage example

--- game/main.py
+++ game/main.py
@@ -1,5 +1,28 @@
+from generals.agents import RandomAgent, ExpanderAgent
+from generals.envs import PettingZooGenerals
+
 def main():
-    print("Hello from game!")
+    random = RandomAgent()
+    expander = ExpanderAgent()
+
+    agent_names = [random.id, expander.id]
+    agents = {
+        random.id: random,
+        expander.id: expander
+    }
+
+    env = PettingZooGenerals(agents=agent_names, render_mode="human")
+    observations, info = env.reset()
+
+    terminated = truncated = False
+    while not (terminated or truncated):
+        actions = {}
+        for agent in env.agents:
+            # Ask agent for action
+            actions[agent] = agents[agent].act(observations[agent])
+        # All agents perform their actions
+        observations, rewards, terminated, truncated, info = env.step(actions)
+        env.render()
 
 
 if __name__ == "__main__":
